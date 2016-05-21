b95f8665c293b6a377520635700b398fe9ad9ec0
Robert Vinluan <robert.vinluan@gmail.com>
2016-05-21 01:04:03 -0400

    tutorial ends

--- tutorial.js
+++ tutorial.js
@@ -13,12 +13,7 @@ var stepArray = [
   },
   {
     text: "We have also given you some letters to use. Please click one of the letters now to see if everything is working as expected.",
-    cede: true
+    cede: false
   }
 ];
 t.addSteps(stepArray);
-
-t.allCompleteCallback = function () {
-  alert('hi');
-}
-// t.startTutorial();
